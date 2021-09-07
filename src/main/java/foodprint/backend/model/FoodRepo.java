e6357116449fa1feb19d1787824a162e44fd6038
cherie lim <cherie@gmail.com>
2021-09-07 14:56:36 +0800

    Updated foodSearch

--- src/main/java/foodprint/backend/model/FoodRepo.java
+++ src/main/java/foodprint/backend/model/FoodRepo.java
@@ -1,5 +1,7 @@
 package foodprint.backend.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -9,7 +11,9 @@ public interface FoodRepo extends JpaRepository<Food, Integer> {
 
     Food findByFoodName(String foodName);
 
-    List<Food> findByFoodNameContains(String FoodName);
+    List<Food> findByFoodNameContains(String foodName);
 
     List<Food> findByFoodNameContainsIgnoreCase(String name);
+
+    Page<Food> findByFoodNameContains(Pageable page, String name);
 }
