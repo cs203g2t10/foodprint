ffa4ce7c40fce80704d80590288a0824a4470f5a
yvonnelhs <yvonnefghijk@gmail.com>
2021-09-14 16:10:41 +0800

    created restaurantDTO

---
+++ src/main/java/foodprint/backend/dto/RestaurantDTO.java
@@ -0,0 +1,24 @@
+package foodprint.backend.dto;
+
+public class RestaurantDTO {
+    private String restaurantName;
+
+    private String restaurantLocation;
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getRestaurantLocation() {
+        return restaurantLocation;
+    }
+
+    public void setRestaurantLocation(String restaurantLocation) {
+        this.restaurantLocation = restaurantLocation;
+    }
+
+}
