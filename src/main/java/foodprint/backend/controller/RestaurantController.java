54fdb7b60e3422cdd204d036ac127f3aa4039659
Daryl Wong <youcanfinddaryl@gmail.com>
2021-09-14 18:54:06 +0800

    allow connection to frontend

--- src/main/java/foodprint/backend/controller/RestaurantController.java
+++ src/main/java/foodprint/backend/controller/RestaurantController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import foodprint.backend.model.FoodRepo;
 
 // REST OpenAPI Swagger - http://localhost:8080/foodprint-swagger.html
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/restaurant")
 public class RestaurantController {
