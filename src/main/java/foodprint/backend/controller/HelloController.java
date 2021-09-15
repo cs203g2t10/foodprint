a3f7e80684e71bb16907f09b66170ffed9bed7b5
Wen Liang Goh <wlgoh.2020@sis.smu.edu.sg>
2021-09-16 01:43:58 +0800

    Implement whoami route, comment out unused imports

--- src/main/java/foodprint/backend/controller/HelloController.java
+++ src/main/java/foodprint/backend/controller/HelloController.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,8 @@ import foodprint.backend.model.User;
 @RestController
 @RequestMapping("/api/v1/hello")
 public class HelloController {
+
+	Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping({"/hello"})
 	@ResponseStatus(code = HttpStatus.OK)
