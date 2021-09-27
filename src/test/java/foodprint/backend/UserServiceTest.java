0eff3874159b8d2059c88410f08373ce562f2491
michtyx <mxchxlle@gmail.com>
2021-09-28 02:22:10 +0800

    more unit testing

--- src/test/java/foodprint/backend/UserServiceTest.java
+++ src/test/java/foodprint/backend/UserServiceTest.java
@@ -45,6 +45,7 @@ public class UserServiceTest {
         
         assertNotNull(savedUser);
         verify(users).findByEmail(user.getEmail());
+        verify(passwordEncoder).encode("SuperSecurePassw0rd");
         verify(users).saveAndFlush(user);
     }
 
@@ -97,4 +98,33 @@ public class UserServiceTest {
         verify(users).findByEmail(user.getEmail());
     }
     
+    @Test
+    void updateUser_NewEmail_ReturnUser() {
+        User user = new User("bobbytan@gmail.com", "SuperSecurePassw0rd", "Bobby Tan");
+        when(users.findByEmail(any(String.class))).thenReturn(Optional.empty());
+        when(users.findById(null)).thenReturn(Optional.of(user));
+        when(passwordEncoder.encode("SuperSecurePassw0rd")).thenReturn("$2a$12$uaTxLl9sPzGbIozqCB0wcuKjmmsZNW2mswGw5VRdsU4XFWs9Se7Uq");
+        when(users.saveAndFlush(any(User.class))).thenReturn(user);
+
+        User updatedUser = userService.updateUser(user.getId(), user);
+
+        assertNotNull(updatedUser);
+        verify(users).findByEmail(user.getEmail());
+        verify(users).findById(user.getId());
+        verify(passwordEncoder).encode("SuperSecurePassw0rd");
+        verify(users).saveAndFlush(user);
+    }
+
+    @Test
+    void deleteUser_UserDoesNotExist_ReturnError() {
+        User user = new User("bobbytan@gmail.com", "SuperSecurePassw0rd", "Bobby Tan");
+        when(users.findById(null)).thenReturn(Optional.empty());
+
+        try {
+            userService.deleteUser(user.getId());
+        } catch (NotFoundException e) {
+            assertEquals("User not found", e.getMessage());
+        }
+        verify(users).findById(user.getId());
+    }
 }
