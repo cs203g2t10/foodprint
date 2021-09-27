4f2cecfd59965b8239e1c04786000e8e1ba88f8a
michtyx <mxchxlle@gmail.com>
2021-09-28 00:00:07 +0800

    Unit testing

--- src/test/java/foodprint/backend/UserServiceTest.java
+++ src/test/java/foodprint/backend/UserServiceTest.java
@@ -13,11 +13,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import foodprint.backend.model.User;
 import foodprint.backend.model.UserRepo;
-import foodprint.backend.service.UserDetailsServiceImpl;
 import foodprint.backend.service.UserService;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,13 +38,12 @@ public class UserServiceTest {
 
         when(users.findByEmail(any(String.class))).thenReturn(Optional.empty());
         when(users.saveAndFlush(any(User.class))).thenReturn(user);
-        when(users.save(any(User.class))).thenReturn(user);
         when(passwordEncoder.encode("SuperSecurePassw0rd")).thenReturn("$2a$12$uaTxLl9sPzGbIozqCB0wcuKjmmsZNW2mswGw5VRdsU4XFWs9Se7Uq");
 
         User savedUser = userService.createUser(user);
         assertNotNull(savedUser);
         verify(users).findByEmail(user.getEmail());
-        verify(users).save(user);
+        verify(users).saveAndFlush(user);
     }
 
     @Test
@@ -60,4 +59,12 @@ public class UserServiceTest {
 
         verify(users).findByEmail(user.getEmail());
     }
+
+    @Test
+    void getUsers_ReturnUsers() {
+        List<User> retrievedUser = userService.getAllUsers();
+        assertNotNull(retrievedUser);
+    }
+
+    
 }
