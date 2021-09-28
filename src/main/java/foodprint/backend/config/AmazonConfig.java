056bf63d62a1859641fe4b3b4e47ed7dbf68f4b3
Wen Liang <wlgoh.2020@sis.smu.edu.sg>
2021-09-28 20:11:47 +0800

    Merge main into uploadImg

--- src/main/java/foodprint/backend/config/AmazonConfig.java
+++ src/main/java/foodprint/backend/config/AmazonConfig.java
@@ -5,17 +5,25 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
+
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String awsAccessKey;
+    
+    @Value("${AWS_SECRET_KEY}")
+    private String awsSecretKey;
+
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials =
-                new BasicAWSCredentials("AKIA6C6PCOXUWLJVUJGT", "sxZ3Wsr1r0aHHNwN07QapuXYp08vC96d2AFj9hm6");
-        return AmazonS3ClientBuilder
-                .standard()
+        AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+
+        return AmazonS3ClientBuilder.standard()
                 .withRegion("ap-southeast-1")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
