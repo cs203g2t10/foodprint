934d0c2b07d007dd4f033e30351eb4798b273b0d
michtyx <mxchxlle@gmail.com>
2021-10-12 21:28:41 +0800

    emailBody changes (for verification & reset pw)

--- src/main/java/foodprint/backend/service/UserService.java
+++ src/main/java/foodprint/backend/service/UserService.java
@@ -120,7 +120,7 @@ public class UserService {
         // Craft and send the email
         String emailBody = String.format(
             "Hi %s, \n\n" +    
-            "You have requested for a password reset! Please use this link to set a new password http://foodprint.works/api/v1/user/auth/resetpwd/%s \n\n" +
+            "You have requested for a password reset! Please use this link to set a new password http://foodprint.works/resetpassword?token=%s \n\n" +
             "This verification token will expire in 48 hours. You can safely ignore this email if you did not request for it. \n\n" +
             "Regards,\nFoodprint Support",
             user.getFirstName(),
