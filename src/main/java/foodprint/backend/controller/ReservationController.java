4f972fb40e7fcdecdc788fa6b0f0a58d347b63a6
michtyx <mxchxlle@gmail.com>
2021-11-02 16:48:01 +0800

    changes

--- src/main/java/foodprint/backend/controller/ReservationController.java
+++ src/main/java/foodprint/backend/controller/ReservationController.java
@@ -238,7 +238,7 @@ public class ReservationController {
             for (LineItemDTO lineItemDTO : dto.getLineItems()) {
                 Food food = restaurantService.getFood(restaurantId, lineItemDTO.getFoodId());
                 int amount = lineItemsHashMap.getOrDefault(food, 0);
-                lineItemsHashMap.put(food, amount + 1);
+                lineItemsHashMap.put(food, amount + lineItemDTO.getQuantity());
             }
 
             List<LineItem> savedLineItems = new ArrayList<>();
