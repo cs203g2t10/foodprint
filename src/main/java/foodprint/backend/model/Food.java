a1c74ec6bdc8dc17c0f10f5e91f2734830cbf827
yvonnelhs <yvonnefghijk@gmail.com>
2021-11-09 13:37:56 +0800

    oops now its rly fixed

--- src/main/java/foodprint/backend/model/Food.java
+++ src/main/java/foodprint/backend/model/Food.java
@@ -15,7 +15,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
