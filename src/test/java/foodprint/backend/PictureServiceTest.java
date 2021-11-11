a13ddf650a4ce2cea75e6d1689d74e11269bbbb2
cherielht <cherie.lim.2020@scis.smu.edu.sg>
2021-11-11 23:10:17 +0800

    removed unused codes

--- src/test/java/foodprint/backend/PictureServiceTest.java
+++ src/test/java/foodprint/backend/PictureServiceTest.java
@@ -1,7 +1,6 @@
 package foodprint.backend;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
