003c7d7bc616bf315af6a7f7069365eb322b22b6
cherielht <cherie.lim.2020@scis.smu.edu.sg>
2021-11-11 22:19:48 +0800

    reservation unit test

--- src/test/java/foodprint/backend/ReservationServiceTest.java
+++ src/test/java/foodprint/backend/ReservationServiceTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -57,6 +58,7 @@ public class ReservationServiceTest {
     private LineItemDTO lineItemDTO;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+    private String date;
     private List<Reservation> reservationList;
     private List<String> restaurantCategories;
 
@@ -150,6 +152,40 @@ public class ReservationServiceTest {
 
         assertEquals(reservationList, result);
         verify(reservations).findByRestaurant(restaurant);
+    }
+
+    @Test
+    void getAllReservationByUser_ReturnList() {
+        reservationList.add(reservation);
+        when(reservations.findByUser(any(User.class))).thenReturn(reservationList);
+
+        List<Reservation> result = reservationService.getAllReservationByUser(user);
+
+        assertEquals(reservationList, result);
+        verify(reservations).findByUser(user);
+    }
+
+    @Test
+    void getUserUpcomingReservations_UserFound_ReturnList() {
+        reservation.setDate(LocalDateTime.now().plusDays(10));
+        reservationList.add(reservation);
+        when(reservations.findByUser(any(User.class))).thenReturn(reservationList);
+
+        List<Reservation> result = reservationService.getUserUpcomingReservations(user);
+
+        assertEquals(reservationList, result);
+        verify(reservations).findByUser(user);
+    }
+
+    @Test
+    void getUserPastReservations_UserFound_ReturnList() {
+        reservation.setDate(LocalDateTime.now().minusDays(10));
+        when(reservations.findByUser(any(User.class))).thenReturn(reservationList);
+
+        List<Reservation> result = reservationService.getUserPastReservations(user);
+
+        assertEquals(reservationList, result);
+        verify(reservations).findByUser(user);
     }
 
     @Test
