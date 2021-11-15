fc93fe6a50ec80fcbb691c941385b0788d3ec1d5
Leow Yi Xuan <yixuan.leow.2020@scis.smu.edu.sg>
2021-11-16 02:18:59 +0800

    2fa integration testing

--- src/test/java/foodprint/backend/TwoFaIntegrationTest.java
+++ src/test/java/foodprint/backend/TwoFaIntegrationTest.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 import javax.persistence.DiscriminatorValue;
 
+import org.jboss.aerogear.security.otp.Totp;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,14 +19,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import foodprint.backend.dto.AdminUserDTO;
 import foodprint.backend.dto.AuthRequestDTO;
 import foodprint.backend.dto.AuthResponseDTO;
-import foodprint.backend.dto.FavouriteRestaurantDTO;
-import foodprint.backend.dto.ManagerRequestDTO;
-import foodprint.backend.dto.UpdateUserDTO;
-import foodprint.backend.model.Restaurant;
-import foodprint.backend.model.RestaurantRepo;
 import foodprint.backend.model.User;
 import foodprint.backend.model.UserRepo;
 
@@ -72,6 +67,7 @@ public class TwoFaIntegrationTest {
         String encodedPassword = new BCryptPasswordEncoder().encode("SuperSecurePassw0rd");
         newUser = new User("bobby@user.com", encodedPassword, "bobby");
         newUser.setRoles("FP_USER");
+        newUser.setTwoFaSecret( "6jm7n6xwitpjooh7ihewyyzeux7aqmw2");
         newUser.setRegisteredOn(LocalDateTime.now());
         userRepo.saveAndFlush(newUser);
     }
@@ -133,6 +129,118 @@ public class TwoFaIntegrationTest {
         
         assertEquals(500, responseEntity.getStatusCode().value());
         
+    }
+
+    @Test
+    public void twoFactorConfirm_Successful() {
+        AuthRequestDTO loginRequest = new AuthRequestDTO();
+        loginRequest.setEmail("bobby@user.com");
+        loginRequest.setPassword("SuperSecurePassw0rd");
+        AuthResponseDTO loginResponse = testRestTemplate.postForObject(createURLWithPort("/api/v1/auth/login"), loginRequest, AuthResponseDTO.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + loginResponse.getToken());
+        System.out.println(loginResponse.getStatus());
+        headers.add("Content-Type", "application/json");
+        Totp totp = new Totp(newUser.getTwoFaSecret());
+        String tok = totp.now();
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(newUser.getEmail(), newUser.getPassword());
+
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(
+                createURLWithPort("/api/v1/twofactor/confirm/{token}"),
+                HttpMethod.POST,
+                new HttpEntity<Object>(headers),
+                String.class, 
+                tok,
+                token.getPrincipal());
+        
+        assertEquals(200, responseEntity.getStatusCode().value());
+    }
+
+    @Test
+    public void twoFactorConfirm_Failure() {
+        AuthRequestDTO loginRequest = new AuthRequestDTO();
+        loginRequest.setEmail("bobby@user.com");
+        loginRequest.setPassword("SuperSecurePassw0rd");
+        AuthResponseDTO loginResponse = testRestTemplate.postForObject(createURLWithPort("/api/v1/auth/login"), loginRequest, AuthResponseDTO.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + loginResponse.getToken());
+        System.out.println(loginResponse.getStatus());
+        headers.add("Content-Type", "application/json");
+        String tok = "12d";
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(newUser.getEmail(), newUser.getPassword());
+
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(
+                createURLWithPort("/api/v1/twofactor/confirm/{token}"),
+                HttpMethod.POST,
+                new HttpEntity<Object>(headers),
+                String.class, 
+                tok,
+                token.getPrincipal());
+        
+        assertEquals(500, responseEntity.getStatusCode().value());
+    }
+
+    @Test
+    public void twoFactorDisable_Success() {
+        AuthRequestDTO loginRequest = new AuthRequestDTO();
+        loginRequest.setEmail("bobby@user.com");
+        loginRequest.setPassword("SuperSecurePassw0rd");
+        AuthResponseDTO loginResponse = testRestTemplate.postForObject(createURLWithPort("/api/v1/auth/login"), loginRequest, AuthResponseDTO.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + loginResponse.getToken());
+        System.out.println(loginResponse.getStatus());
+        headers.add("Content-Type", "application/json");
+        Totp totp = new Totp(newUser.getTwoFaSecret());
+        String tok = totp.now();
+        newUser.setTwoFaSet(true);
+        userRepo.saveAndFlush(newUser);
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(newUser.getEmail(), newUser.getPassword());
+
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(
+                createURLWithPort("/api/v1/twofactor/disable/{token}"),
+                HttpMethod.POST,
+                new HttpEntity<Object>(headers),
+                String.class, 
+                tok,
+                token.getPrincipal());
+        
+        assertEquals(200, responseEntity.getStatusCode().value());
+    }
+
+    @Test
+    public void twoFactorDisable_Failure() {
+        AuthRequestDTO loginRequest = new AuthRequestDTO();
+        loginRequest.setEmail("bobby@user.com");
+        loginRequest.setPassword("SuperSecurePassw0rd");
+        AuthResponseDTO loginResponse = testRestTemplate.postForObject(createURLWithPort("/api/v1/auth/login"), loginRequest, AuthResponseDTO.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("Authorization", "Bearer " + loginResponse.getToken());
+        System.out.println(loginResponse.getStatus());
+        headers.add("Content-Type", "application/json");
+        String tok = "12d";
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(newUser.getEmail(), newUser.getPassword());
+
+        ResponseEntity<String> responseEntity = testRestTemplate.exchange(
+                createURLWithPort("/api/v1/twofactor/disable/{token}"),
+                HttpMethod.POST,
+                new HttpEntity<Object>(headers),
+                String.class, 
+                tok,
+                token.getPrincipal());
+        
+        assertEquals(500, responseEntity.getStatusCode().value());
     }
 
     private String createURLWithPort(String uri) {
